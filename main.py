07fb25aa3350956852c22ab0cfec972f6c29a333
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2024-02-04 01:02:18 +0900

    lint main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import re
-import os
 import logging
+import os
 import random
+import re
+
 from slack_bolt import App
 
 logging.basicConfig(level=logging.DEBUG)
@@ -13,13 +14,19 @@ app = App(
     process_before_response=True,
 )
 
+
 @app.message(re.compile("([a-zA-Z0-9-]+)\+\+"))
 def handle_message(message, say, context):
-    for matched_text in context['matches']:
-        patterns = (f"{matched_text}がプラプラされたよ. ", f"{matched_text} いいね!", f"{matched_text} ナイス!")
+    for matched_text in context["matches"]:
+        patterns = (
+            f"{matched_text}がプラプラされたよ. ",
+            f"{matched_text} いいね!",
+            f"{matched_text} ナイス!",
+        )
         counter = random.randint(0, 100)
         reply = random.choice(patterns) + f" (++された回数: {counter})"
         say(reply)
+
 
 # @app.message("hello")
 # def message_hello(message, say):
@@ -27,6 +34,7 @@ def handle_message(message, say, context):
 
 if __name__ == "__main__":
     from slack_bolt.adapter.socket_mode import SocketModeHandler
+
     SocketModeHandler(app, os.getenv("SLACK_APP_TOKEN")).start()
 
 from slack_bolt.adapter.aws_lambda import SlackRequestHandler
