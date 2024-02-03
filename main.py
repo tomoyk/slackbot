04565286c68dab96eb2d553d9dcd60a02962453b
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2024-02-04 01:01:40 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,43 +1,41 @@
 import re
 import os
 import logging
+import random
 from slack_bolt import App
 
 logging.basicConfig(level=logging.DEBUG)
 
-# Slack Appの設定
+# Slack App Config
 app = App(
     token=os.getenv("SLACK_BOT_TOKEN"),
     signing_secret=os.getenv("SLACK_SIGNING_SECRET"),
     process_before_response=True,
 )
 
-# メッセージイベントの処理
 @app.message(re.compile("([a-zA-Z0-9-]+)\+\+"))
 def handle_message(message, say, context):
-    print("Triggered")
-    matched_text = context['matches'][0]  # 正規表現にマッチしたテキスト
-    say(f"Detected increment for: {matched_text}")
+    for matched_text in context['matches']:
+        patterns = (f"{matched_text}がプラプラされたよ. ", f"{matched_text} いいね!", f"{matched_text} ナイス!")
+        counter = random.randint(0, 100)
+        reply = random.choice(patterns) + f" (++された回数: {counter})"
+        say(reply)
 
-@app.message("hello")
-def message_hello(message, say):
-    # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
-    say(f"Hey there <@{message['user']}>!")
+# @app.message("hello")
+# def message_hello(message, say):
+#     say(f"Hey there <@{message['user']}>!")
 
 if __name__ == "__main__":
-    # python app.py のように実行すると開発用Webサーバーで起動
-    # app.start()
     from slack_bolt.adapter.socket_mode import SocketModeHandler
     SocketModeHandler(app, os.getenv("SLACK_APP_TOKEN")).start()
 
 from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 
-# ロギングを AWS Lambda 向けに初期化します
+# Logging for AWS Lambda
 SlackRequestHandler.clear_all_log_handlers()
 logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
 
+# AWS Lambda support
 def lambda_handler(event, context):
-    # AWS Lambda用のハンドラーを初期化
     slack_handler = SlackRequestHandler(app)
-    # AWS LambdaからのイベントをSlack Bolt Appに渡して処理
     return slack_handler.handle(event, context)
