f5c42d2b8a60c5d91da55362e0f2867af2a0172b
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2024-02-07 15:38:26 +0900

    add plus plus feature

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 import random
 import re
 
+import boto3
 from slack_bolt import App
 
 logging.basicConfig(level=logging.DEBUG)
@@ -19,17 +20,42 @@ app = App(
     process_before_response=True,  # Required for Lambda
 )
 
+# Connect to DynamoDB
+dynamodb = boto3.resource('dynamodb')
+DYNAMODB_TABLE_NAME="tmykbot-slack-dev-tab"
+table = dynamodb.Table(os.getenv("DYNAMODB_TABLE_NAME", DYNAMODB_TABLE_NAME))
 
 @app.message(re.compile("([a-zA-Z0-9-]+)\+\+"))
 def handle_message(message, say, context):
     for matched_text in context["matches"]:
+        try:
+            response = table.get_item(
+                Key={
+                    'alias': matched_text
+                }
+            )
+
+            if (item := response.get("Item")) is None: # Key does not exist
+                total_count = 1
+            else: # Key exists
+                total_count = item.get("count", 0) + 1
+            table.put_item(
+                Item={
+                    'alias': matched_text,
+                    'count': total_count,
+                }
+            )
+        except Exception as e:
+            print(e)
+            say("Error:" + e)
+            continue
+
         patterns = (
             f"{matched_text}がプラプラされたよ. ",
             f"{matched_text} いいね!",
             f"{matched_text} ナイス!",
         )
-        counter = random.randint(0, 100)
-        reply = random.choice(patterns) + f" (++された回数: {counter})"
+        reply = random.choice(patterns) + f" (++された回数: {total_count})"
         say(reply)
 
 
