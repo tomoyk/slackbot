eb703f7663e0d0d87e72c560b78d6c55b653f0f2
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2024-02-08 14:25:16 +0900

    add !word support

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ table = dynamodb.Table(os.getenv("DYNAMODB_TABLE_NAME", DYNAMODB_TABLE_NAME))
 
 
 @app.message(re.compile("([a-zA-Z0-9-]+)\+\+"))
-def handle_message(message, say, context):
+def plus_plus_handler(message, say, context):
     for matched_text in context["matches"]:
         try:
             response = table.get_item(Key={"alias": matched_text})
@@ -54,6 +54,40 @@ def handle_message(message, say, context):
         )
         reply = random.choice(patterns) + f" (++された回数: {total_count})"
         say(reply)
+
+
+def word_not_found(text, say):
+    patterns = (
+        f"ごめんね. 「{text}」はわからないよ.",
+        f"「{text}」はわからないよ.",
+        f"恐れ入りますが「{text}」に関して当方ではわかりかねます.",
+    )
+    reply = random.choice(patterns)
+    say(reply)
+
+
+@app.message(re.compile("!word ([^ ]+)"))
+def word_handler(message, say, context):
+    for matched_text in context["matches"]:
+        try:
+            response = table.get_item(Key={"alias": matched_text})
+            if (item := response.get("Item")) is None:  # Key does not exist
+                word_not_found(text=matched_text, say=say)
+                continue
+
+            # Key exists
+            if (desc := item.get("description")) is None:
+                word_not_found(text=matched_text, say=say)
+                continue
+
+            # Word found
+            reply = f"{matched_text}: {desc}"
+            say(reply)
+
+        except Exception as e:
+            print(e)
+            say("Error:" + e)
+            continue
 
 
 if __name__ == "__main__":
